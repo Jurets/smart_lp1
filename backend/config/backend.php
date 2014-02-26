3d6d2ecd2944df198a4c45a4448a79824ea509dc
fedman <fedmanbfg@mail.ru>
2014-02-26 18:50:24 +0200

    template + user module

---
+++ backend/config/backend.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ *
+ * backend.php configuration file
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @copyright 2013 2amigOS! Consultation Group LLC
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+defined('APP_CONFIG_NAME') or define('APP_CONFIG_NAME', 'backend');
+
+// web application configuration
+return array(
+	'name' => '{APPLICATION NAME}',
+	'basePath' => realPath(__DIR__ . '/..'),
+	// path aliases
+	'aliases' => array(
+		'bootstrap' => dirname(__FILE__) . '/../..' . '/common/lib/vendor/2amigos/yiistrap',
+		'yiiwheels' =>  dirname(__FILE__) . '/../..' . '/common/lib/vendor/2amigos/yiiwheels'
+	),
+
+	// application behaviors
+	'behaviors' => array(),
+
+	// controllers mappings
+	'controllerMap' => array(),
+
+	// application modules
+	'modules' => array(
+        'user'=>array(
+            'class'=>'backend.modules.user.UserModule',
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+
+            # send activation email
+            'sendActivationMail' => true,
+
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => false,
+
+            # automatically login from registration
+            'autoLogin' => true,
+
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+
+            # login form path
+            'loginUrl' => array('/user/login'),
+
+            # page after login
+            'returnUrl' => array('/user/profile'),
+
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
+    ),
+    
+    'import'=>array(
+        'application.models.*',
+        'application.components.*',
+        'application.modules.user.models.*',
+        'application.modules.user.components.*',
+    ),
+
+	// application components
+	'components' => array(
+
+		'bootstrap' => array(
+			'class' => 'bootstrap.components.TbApi',
+		),
+
+		'clientScript' => array(
+			'scriptMap' => array(
+				'bootstrap.min.css' => false,
+				'bootstrap.min.js' => false,
+				'bootstrap-yii.css' => false
+			)
+		),
+		'urlManager' => array(
+			// uncomment the following if you have enabled Apache's Rewrite module.
+			'urlFormat' => 'path',
+			'showScriptName' => false,
+
+			'rules' => array(
+				// default rules
+				'<controller:\w+>/<id:\d+>' => '<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+				'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+			),
+		),
+        'user'=>array(
+            // enable cookie-based authentication
+            'class' => 'WebUser',
+        ),
+		'errorHandler' => array(
+			'errorAction' => 'site/error',
+		)
+	),
+);
