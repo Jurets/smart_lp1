9f6a92e9c9399e9648c18e30d502b68948a27cd3
Jurets <jurets75@gmail.com>
2014-02-27 14:22:59 +0200

    * модуль юзер в коммон

--- common/config/main.php
+++ common/config/main.php
@@ -59,5 +59,42 @@ return array(
 		// php configuration
 		'php.defaultCharset' => 'utf-8',
 		'php.timezone'       => 'UTC',
-	)
+	),
+    
+    // application modules
+    'modules' => array(
+        'user'=>array(
+            'class'=>'common.modules.user.UserModule',
+            # encrypting method (php hash function)
+            'hash' => 'md5',
+
+            # send activation email
+            'sendActivationMail' => true,
+
+            # allow access for non-activated users
+            'loginNotActiv' => false,
+
+            # activate user on registration (only sendActivationMail = false)
+            'activeAfterRegister' => false,
+
+            # automatically login from registration
+            'autoLogin' => true,
+
+            # registration path
+            'registrationUrl' => array('/user/registration'),
+
+            # recovery password path
+            'recoveryUrl' => array('/user/recovery'),
+
+            # login form path
+            'loginUrl' => array('/user/login'),
+
+            # page after login
+            'returnUrl' => array('/user/profile'),
+
+            # page after logout
+            'returnLogoutUrl' => array('/user/login'),
+        ),
+    ),
+    
 );
