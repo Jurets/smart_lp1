96dc935ea57bc29a88388f6a30c36f524c9ad182
Jurets <jurets75@gmail.com>
2014-04-15 15:48:37 +0300

    * изменения настроечных файлов

--- backend/www/index.php
+++ backend/www/index.php
@@ -10,14 +10,13 @@
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
 require('./../../common/lib/vendor/autoload.php');
-require('./../../common/lib/vendor/yiisoft/yii/framework/yii.php');
-
-Yii::setPathOfAlias('Yiinitializr', './../../common/lib/Yiinitializr');
 
 use Yiinitializr\Helpers\Initializer;
 
 Initializer::create('./../', 'backend', array(
 	__DIR__ .'/../../common/config/main.php',
-	__DIR__ .'/../../common/config/env.php',
+    __DIR__ .'/../../common/config/env.php',
+    __DIR__ .'/../../common/config/env/dev.php',
+	//__DIR__ .'/../../common/config/env/prod.php',
 	__DIR__ .'/../../common/config/local.php',
 ))->run();
--- frontend/www/index.php
+++ frontend/www/index.php
@@ -10,12 +10,8 @@
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
 require('./../../common/lib/vendor/autoload.php');
-require('./../../common/lib/vendor/yiisoft/yii/framework/yii.php');
-
-Yii::setPathOfAlias('Yiinitializr', './../../common/lib/Yiinitializr');
 
 use Yiinitializr\Helpers\Initializer;
-
 
 Initializer::create('./../', 'frontend', array(
 	__DIR__ .'/../../common/config/main.php',
