43e161f105f0d2e9fbfdb5ef1914f4c15d0fb85b
andrew <andrew@smart.(none)>
2014-05-22 19:21:02 +0300

    im part 2

---
+++ console/migrations/m140522_142801_itemsstorage.php
@@ -0,0 +1,19 @@
+<?php
+
+class m140522_142801_itemsstorage extends CDbMigration
+{
+	public function up()
+	{		
+		$this->createTable('itemsstorage', array(
+			'item'=>'varchar(255) not null',
+			'content'=>'blob',
+			'PRIMARY KEY (`item`)',
+		), 'ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+		);
+	}
+
+	public function down()
+	{
+		$this->dropTable('itemsstorage');
+	}
+}
