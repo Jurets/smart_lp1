0a1ba7c7cff0d130975daaabacc40e5432e87270
andrew pernalsky <pernalsky@gmail.com>
2014-05-23 03:13:34 +0300

    im промежуточная версия

--- backend/models/Indexmanager.php
+++ backend/models/Indexmanager.php
@@ -1,15 +1,13 @@
 <?php
-
 class Indexmanager extends CFormModel {
-    public $videoLink; // линк на видеоролик в youtube
+    public $videolink; // линк на видеоролик в youtube
     public $about; // контейнер для текст-контента "О системе"
-    public $sliderList; // массив пар: линк на upload-фото и text-описание
-    private jsonResult
-    private const INDEX_ITEM = 'indexmanagement'; // первичный ключ для json-объекта у 
+    public $sliderlist; // массив пар: линк на upload-фото и text-описание
+    private $jsonResult;
     
     public function rules(){
         return array(
-            //array('videolink', 'about', ),
+            array('videolink', 'about', 'sliderlist', 'safe' ),
         );
     }
 }
---
+++ backend/modules/im/views/default/_form.php
@@ -0,0 +1,17 @@
+<div class="form">
+
+    <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+	'id'=>'indexmanager-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+<?php echo $form->errorSummary($model); ?>
+  <?php // TO DO ?>  
+  <?php // TO DO ?>  
+  <?php // TO DO -array- ?>  
+<?php $this->endWidget(); ?>
+</div>
