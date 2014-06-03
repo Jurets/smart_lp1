ae9ec242873a07cfd27b43f99be568444d2d7151
mix-fGt <tka4enkoegor@gmail.com>
2014-06-03 11:38:28 +0300

    test

--- backend/modules/faq/controllers/FaqController.php
+++ backend/modules/faq/controllers/FaqController.php
@@ -60,6 +60,7 @@ class FaqController extends EController
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
 	 */
+
 	public function actionCreate()
 	{
 		$model=new Faq;
