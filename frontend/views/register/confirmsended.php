74d70def1b3b03a58d5b6dc5f95935ed4d43c738
Jurets <jurets75@gmail.com>
2014-06-07 12:18:34 +0300

    http://mantis.cmapt.com.ua/view.php?id=1261 + вьюшка для окончания шага 1 (подтв. емейла)

---
+++ frontend/views/register/confirmsended.php
@@ -0,0 +1,5 @@
+<p id="shag-1-3-text" >
+    На вашу почту было выслано письмо. В письме вы найдете ссылку Нажмите на нее. <a href="#" ><br><br>ЕСЛИ ВЫ НЕ ПОЛУЧИЛИ ПИСЬМО</a> 
+</p>
+
+<a href="shag-1-4.html"><input type="button" name="btn"  class="btn-style-gray" value="ДАЛЕЕ" /></a>
