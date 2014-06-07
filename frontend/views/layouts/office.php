75e008ff568484d95832bf0c2e791a109d737a27
Jurets <jurets75@gmail.com>
2014-06-07 11:44:58 +0300

    офис * убрать лишнюю менюшку

--- frontend/views/layouts/office.php
+++ frontend/views/layouts/office.php
@@ -28,30 +28,6 @@ $this->beginContent('//layouts/common');
 
             </div>
 
-            
-            
-<div id="divMenu">
-    <?php $this->widget('zii.widgets.CMenu',array(
-        'id'=>'nav2',
-        'activeCssClass'=>'myitem-active',
-        'items'=>array(
-            array('label'=>'СТАТИСТИКА', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style1')),
-            array('label'=>'СТРУКТУРА', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style2')),
-            array('label'=>'НОВОСТИ', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style3')),
-            array('label'=>'ЧАТ', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style4')),
-            array('label'=>'ПРИГЛАШЕНИЕ', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style5')),
-            array('label'=>'НАСТРОЙКИ', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style6')),
-            array('label'=>'ПОМОЩЬ', 'url'=>array(''),'itemOptions'=>array('class'=>'btn-style7')),
-            array('label'=>'ПРАВИЛА', 'url'=>array('office/specification'),'itemOptions'=>array('class'=>'btn-style8'))
-
-        ,)
-    )); ?>
-
-</div>
-<div id="BottomOfficeLine"></div>            
-            
-            
-            
             <div id="content">
 
                 <div id="divMenu">
