b5ef477e952213f9c69b45abb82c7113028da39d
mix-fGt <tka4enkoegor@gmail.com>
2014-06-13 16:56:33 +0300

    update

--- frontend/views/office/settings.php
+++ frontend/views/office/settings.php
@@ -4,6 +4,12 @@
  */
 Yii::app()->clientScript->registerCssFile('/css/style-office.css');
 //Yii::app()->clientScript->registerScriptFile('/js/jquery-ui.min.js');
+$form = $this->beginWidget('CActiveForm', array(
+    'id'=>'settings-form',
+    'enableAjaxValidation'=>false,
+    'htmlOptions' => array('enctype'=>'multipart/form-data'),
+    //'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
+));
 ?>
 <div id="office-6-content">
     <div><a  id="logo" href="index.html"> </a></div>
@@ -14,12 +20,18 @@ Yii::app()->clientScript->registerCssFile('/css/style-office.css');
 
 
 
-           <p class="sub1">ИМЯ ПОЛЬЗОВАТЕЛЯ:</p>
-            <input class="textbox1" type="text" value="">
-            <p class="sub2">ИМЯ*:</p>
-            <input class="textbox2" type="text">
-           <p class="sub2-1">ФАМИЛИЯ*:</p>
-            <input class="textbox2-1" type="text">
+
+        <p class="sub1">ИМЯ ПОЛЬЗОВАТЕЛЯ:</p>
+        <?php echo $form->textField($participant, 'username', array('class'=>'textbox1')); //ИМЯ ПОЛЬЗОВАТЕЛЯ ?>
+        <?php echo $form->error($participant, 'username', array('class'=>'error-message em-1')); //ИМЯ ПОЛЬЗОВАТЕЛЯ ?>
+
+        <p class="sub2">ИМЯ*:</p>
+        <?php echo $form->textField($participant, 'first_name', array('class'=>'textbox2')); //ИМЯ ?>
+        <?php echo $form->error($participant, 'first_name', array('class'=>'error-message em-2')); //ИМЯ ?>
+
+        <p class="sub2-1">ФАМИЛИЯ*:</p>
+        <?php echo $form->textField($participant, 'last_name', array('class'=>'textbox2-1')); //ФАМИЛИЯ ?>
+        <?php echo $form->error($participant, 'last_name', array('class'=>'error-message em-3')); //ФАМИЛИЯ ?>
 
 
 
@@ -53,15 +65,21 @@ Yii::app()->clientScript->registerCssFile('/css/style-office.css');
 
                     <p class="shag-1-1-option1-1text">  SKYPE:</p>
              <input type="checkbox" name="checkboxG5" id="checkboxG53" class="css-checkbox3" checked="checked"/><label for="checkboxG53" class="css-label3"></label>
-           <a href="#"  class="vopros1-1" title="разрешить показывать всем пользователям"></a>
-          <input class="shag-1-1-option1-1" type="text">
+        <a href="#"  class="vopros1-1" title="разрешить показывать всем пользователям"></a>
+        <?php echo $form->textField($participant, 'skype', array('class'=>'shag-1-1-option1-1')); //skype ?>
+        <?php echo $form->error($participant, 'skype', array('class'=>'error-message em-0')); //skype ?>
+<!--          <input class="shag-1-1-option1-1" type="text">-->
 
 
 
             <p class="sub1-1">VIBER / МОБИЛЬНЫЙ*:</p>
-            <input class="textbox1-1" type="text">
+<!--            <input class="textbox1-1" type="text">-->
+            <?php echo $form->textField($participant, 'phone', array('class'=>'textbox1-1')); //VIBER / МОБИЛЬНЫЙ ?>
+            <?php echo $form->error($participant, 'phone', array('class'=>'error-message em-0')); //VIBER / МОБИЛЬНЫЙ ?>
             <p class="sub2-3">ТЕКУЩИЙ ПАРОЛЬ*:</p>
-            <input class="textbox2-3" type="password">
+<!--            <input class="textbox2-3" type="password">-->
+            <?php echo $form->textField($participant, 'password', array('class'=>'textbox2-3')); //ТЕКУЩИЙ ПАРОЛЬ ?>
+            <?php echo $form->error($participant, 'password', array('class'=>'error-message em-0')); //ТЕКУЩИЙ ПАРОЛЬ ?>
            <p class="sub2-4">НОВЫЙ ПАРОЛЬ*:</p>
             <input class="textbox2-4" type="password">
             <p class="sub2-5">НОВЫЙ КОШЕЛЕК:</p>
@@ -112,8 +130,11 @@ Yii::app()->clientScript->registerCssFile('/css/style-office.css');
               <div id="shag-1-1-avatar"></div>
           <a href="#"><div id="shag-1-1-vibrat">ВЫБРАТЬ ИЗОБРАЖЕНИЕ</div></a>
 
+         <?php echo CHTML::submitButton('СОХРАНИТЬ',array('class'=>'btn-style-green','name'=>'btn')) ?>
 
-          <a href="#"><input type="button" name="btn"  class="btn-style-green" value="СОХРАНИТЬ" /></a>
+
+        <?php $this->endWidget(); ?>
+<!--          <a href="#"><input type="button" name="btn"  class="btn-style-green" value="СОХРАНИТЬ" /></a>-->
            <div id="popup-1" class="p-6-popup"><span>wddwdw fd fd df df</span><img class="stick" src="images/popupstick.png" width="13"></div>
            <div id="popup-2" class="p-6-popup"><span>wddwdw gfgfgfd ffgfd df df</span><img class="stick" src="images/popupstick.png" width="13"></div>
            <div id="popup-3" class="p-6-popup"><span>wddwdw fd fdfgffg df dfgfgfgf</span><img class="stick" src="images/popupstick.png" width="13"></div>
@@ -133,66 +154,3 @@ Yii::app()->clientScript->registerCssFile('/css/style-office.css');
         <div class="wrap"></div>
 
     </div>
-    <div class="footer">
-
-
-        <div id="footer">
-
-            <div id="footerUL" >
-          <ul id="footerUL1">
-                    <li> <a href="#" id="zagolovok"> Live </a> </li>
-                    <li> <a href="#"> Новости </a> </li>
-                    <li> <a href="#"> Отзывы участников</a> </li>
-                    <li> <a href="#"> Канал на YouTube </a> </li>
-         </ul>
-
-         <ul id="footerUL2">
-                    <li> <a href="#" id="zagolovok"> О компании </a> </li>
-                    <li> <a href="#"> О проекте </a> </li>
-                    <li> <a href="#"> Наши партнеры </a> </li>
-                    <li> <a href="#"> Контакты и Реквизиты </a> </li>
-         </ul>
-            <ul id="footerUL3">
-                    <li> <a href="#" id="zagolovok"> Сервис </a> </li>
-                    <li> <a href="#"> Финансовая помощь </a> </li>
-                    <li> <a href="#"> Бизнес Автомат</a> </li>
-                    <li> <a href="#"> Совместная реклама </a> </li>
-                    <li> <a href="#"> Личный сайт </a> </li>
-                    <li> <a href="#"> SMS-рассылка </a> </li>
-         </ul>
-
-
-            <ul id="footerUL4">
-                    <li> <a href="#" id="zagolovok"> Правила </a> </li>
-                    <li> <a href="#"> Соглашение об участии </a> </li>
-                    <li> <a href="#"> Принципы сообщества</a> </li>
-                    <li> <a href="#"> АнтиСПАМ политика </a> </li>
-         </ul>
-
-
-            <ul id="footerUL5">
-                    <li> <a href="#" id="zagolovok"> Сотрудничество</a> </li>
-                    <li> <a href="#"> Участникам </a> </li>
-                    <li> <a href="#"> Платежные системы</a> </li>
-                    <li> <a href="#"> Обменникам </a> </li>
-                    <li> <a href="#"> Рекламным площадкам </a> </li>
-                    <li> <a href="#"> Стать реселлером </a> </li>
-         </ul>
-
-
-            <ul id="footerUL6">
-                    <li> <a href="#" id="zagolovok"> Служба поддержки</a> </li>
-                    <li> <a href="#"> Инструкции </a> </li>
-                    <li> <a href="#"> Форум </a> </li>
-                    <li> <a href="#"> Связаться с нами</a> </li>
-            </ul>
-
-
-          </div>
-
-            <div><a id="miniLogo"  href="#"></a></div>
-            <p> <a   id="endText"  href="#">© 2014 Всемирна Благотворительная Система JMWS LLC POLAND</a></p>
-
-        </div>
-        <div id="footer-bark-bg"></div>
-        </div>
