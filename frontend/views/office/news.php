f94a561a6533be3e1877faea41c3dc69c20b40f8
andrew <andrew@smart.(none)>
2014-06-17 11:12:28 +0300

    Восстановление новостей после слияния

---
+++ frontend/views/office/news.php
@@ -0,0 +1,72 @@
+<style>
+    .pagination {
+        margin-top: 200px;
+        color: #007e97 !important;
+    }
+    .nextm a {
+        color: #007e97 !important;
+        border: 0 none !important;
+        font-size: 18px;
+    }
+    .prevm a {
+        color: #007e97 !important;
+        border: 0 none !important;
+        font-size: 18px;
+    }
+    
+    .internalm a{
+        color: #007e97 !important;
+        font-size: 18px;
+    }
+    ul.yiiPager .selected a {
+        background: none repeat scroll 0 0 #007e97;
+        color: #FFFFFF;
+        font-weight: bold;
+        
+    }
+   
+    ul.yiiPager .page a {
+    font-weight: bold;
+    font-size: 18px;
+}
+.attended {
+    background: #f4dce3;
+}
+    
+</style>
+
+
+<div id="office3-content">
+<div>
+    
+<?php foreach ($models as $ind=>$model) { ?>
+    <a href="/office/news/<?php echo $model->id . $page?>">
+   <div id="office-3-post<?php echo $ind + 1; ?>" class='<?=$model->attended?>'>
+       
+   <?php echo TbHtml::tag('div', array('style'=>'background-image: url(/admin/uploads/'.'resized-'.$model->image, 'id'=>'blogImg'.($ind+1)), '&nbsp;'); ?>
+       <h4 class="office-3-miniZagolovok"><?php echo Yii::app()->dateFormatter->formatDateTime($model->created, 'medium', null) .', '. Yii::app()->dateFormatter->formatDayInWeek('EEEE', array('wday'=>date('N', strtotime($model->created))));?></h4>
+    <a href="/office/news/<?php echo $model->id . $page?>" class="office-3-zagolovok"><span style="text-transform:uppercase;"><?php echo $model->title ?></span></a>
+       <p class="office-3-text">
+           <?php echo $model->getPrew(); ?>
+       </p>         
+        <hr  width="421" size="1"  color="#838181" />
+   </div>
+</a>
+<?php } ?>
+    
+</div>
+    
+<div class="pagination">
+ <?php 
+ if(isset($pages)){
+   $obj =  $this->widget('CLinkPager', array(
+    'pages' => $pages,
+    'nextPageCssClass' => 'nextm',
+    'previousPageCssClass' => 'prevm',
+    'prevPageLabel' => '<',
+    'nextPageLabel'=>'>'   
+    ));
+ }
+ ?>
+</div>
+</div>
---
+++ frontend/views/office/newsone.php
@@ -0,0 +1,14 @@
+<div id="office3-content">
+    <a href="/office/news/<?php echo $page ?>"><input type="button" name="btn"  class="btn-style-green" value="НАЗАД" /></a>
+         
+        <div id="office-3-1post1">
+            <?php echo TbHtml::tag('div', array('style'=>'background-image: url(/admin/uploads/'.'origin-'.$model->image, 'id'=>'blogImg1'), '&nbsp;'); ?>
+            <h4 class="office-3-miniZagolovok"><?php echo Yii::app()->dateFormatter->formatDateTime($model->created, 'medium', null) .', '. Yii::app()->dateFormatter->formatDayInWeek('EEEE', array('wday'=>date('N', strtotime($model->created))));?></h4>
+            <p class="office-3-zagolovok"><span style="text-transform:uppercase;"><?php echo $model->title ?></span></p>
+            <p class="office-3-text">
+                <?php echo $model->content ?>
+            </p>   
+      </div>
+</div>
+
+<?php $model->attendedUpdate($model->id) ?>
