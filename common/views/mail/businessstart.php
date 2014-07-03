e2782c8917615a318340a9b64c0a8f1a469e22af
kiyanov <kiyanov.alex@gmail.com>
2014-07-03 16:36:26 +0300

    Localization
    Signed-off-by: 'kiyanov' <'kiyanov.alex@gmail.com'>

--- common/views/mail/businessstart.php
+++ common/views/mail/businessstart.php
@@ -1,7 +1,8 @@
 <p style="color: #000; font-size: medium;">
-    Здравствуйте!<br/>
-    Поздравляем с покупкой Бизнес-Старта! Регистрация полностью пройдена. Теперь для входа будут использованы исключительно ваши емейл и пароль.
+    <?php echo Yii::t('common', 'Hello!') ?><br/>
+    <?php echo Yii::t('common', 'Congratulations on your purchase Business Start! Registration is fully passed.') ?>
+    <?php echo Yii::t('common', 'Now login information will be used only your email and password.') ?>
 </p>
-<p>С уважением, <br/>
-администрация сайта <br/>
-<a target="_blank" href=""><?=CHtml::encode(Yii::app()->name)?></a></p>
+<p><?php echo Yii::t('common', 'Best Regards') ?>, <br/>
+    <?php echo Yii::t('common', 'administration') ?> <br/>
+    <a target="_blank" href=""><?= CHtml::encode(Yii::app()->name) ?></a></p>
--- common/views/mail/regconfirm.php
+++ common/views/mail/regconfirm.php
@@ -1,12 +1,12 @@
 <?php
-    $url = Yii::app()->createAbsoluteUrl('register/activate?activkey=' . $participant->activkey);
+$url = Yii::app()->createAbsoluteUrl('register/activate?activkey=' . $participant->activkey);
 ?>
 <p style="color: #000; font-size: medium;">
-    Здравствуйте!<br/>
-    Для подтверждения регистрации перейдите по ссылке <a href="<?php echo $url; ?>" target="_blank"><?php echo $url; ?></a><br/>
-    Не даляйте это письмо, пока окончательно не зарегистрируетесь.
-    Эта ссылка уникальна и будет действовать, пока вы не пройдете все этапы регистрации
+    <?php echo Yii::t('common', 'Hello!') ?><br/>
+    <?php echo Yii::t('common', 'To confirm registration follow the link') ?> <a href="<?php echo $url; ?>" target="_blank"><?php echo $url; ?></a><br/>
+    <?php echo Yii::t('common', 'Do not remove this message until you finally registered.') ?>
+    <?php echo Yii::t('common', 'This link is unique and will operate until you pass all registration steps') ?>
 </p>
-<p>С уважением, <br/>
-администрация сайта <br/>
-<a target="_blank" href=""><?=CHtml::encode(Yii::app()->name)?></a></p>
+<p><?php echo Yii::t('common', 'Best Regards') ?>, <br/>
+    <?php echo Yii::t('common', 'administration') ?> <br/>
+    <a target="_blank" href=""><?= CHtml::encode(Yii::app()->name) ?></a></p>
