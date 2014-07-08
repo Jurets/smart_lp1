88de1d4e743a213b6649b962579ac0d06c2c53be
andrew <andrew@smart.(none)>
2014-07-08 11:27:36 +0300

    Perfect Money Вариант транзакционных запросов №1

--- common/extensions/PerfectMoney/models/PerfectMoney.php
+++ common/extensions/PerfectMoney/models/PerfectMoney.php
@@ -88,86 +88,103 @@ class PerfectMoney extends CFormModel {
   /* Служебные вспомогательные методы */
   private function confirmSuccessHelper($event){
       $dbObject = Yii::app()->db;
-      $kind_id = false;
-      if(isset($this->transactionId) && is_int($this->transactionId)){ //установка поля tr_kind_id вручную
-          $act_id = $this->transactionId;
-      }else{ // установка поля tr_kind_id автоматически
-            $isExist = $dbObject->createCommand()
-            ->select('kind_id')
-            ->from('pm_transaction_kind')
-           ->where('description=:desc', array(':desc'=>$this->transactionKind))
-            ;
-        $read = $isExist->query();
-        $kind_id = $read->read();
-        if($kind_id == false){
-             $dbObject->createCommand()
-             ->insert('pm_transaction_kind', array('description'=>$this->transactionKind));
-             $act_id = Yii::app()->db->getLastInsertID();
-        }else{
-             $act_id = $kind_id['kind_id'];
+      $transaction = $dbObject->beginTransaction();
+      try {
+        $kind_id = false;
+        if(isset($this->transactionId) && is_int($this->transactionId)){ //установка поля tr_kind_id вручную
+            $act_id = $this->transactionId;
+        }else{ // установка поля tr_kind_id автоматически
+              $isExist = $dbObject->createCommand()
+              ->select('kind_id')
+              ->from('pm_transaction_kind')
+             ->where('description=:desc', array(':desc'=>$this->transactionKind))
+              ;
+          $read = $isExist->query();
+          $kind_id = $read->read();
+          if($kind_id == false){
+               $dbObject->createCommand()
+               ->insert('pm_transaction_kind', array('description'=>$this->transactionKind));
+               $act_id = Yii::app()->db->getLastInsertID();
+          }else{
+               $act_id = $kind_id['kind_id'];
+          }
+
         }
-      
-      }
-      $dbObject->createCommand()
-        ->insert('pm_transaction_log', array(
-           'from_user_id'=>$this->payerId,
-           'from_purse'=>$this->payerAccount,
-           'to_user_id'=>$this->payeeId,
-           'to_purse'=>$this->payeeAccount,
-           'notation'=>$this->notation,
-           'amount'=>$this->amount,
-           'currency'=>'USD',
-           'tr_kind_id'=>$act_id,
-        ));
-  }
+        $dbObject->createCommand()
+          ->insert('pm_transaction_log', array(
+             'from_user_id'=>$this->payerId,
+             'from_purse'=>$this->payerAccount,
+             'to_user_id'=>$this->payeeId,
+             'to_purse'=>$this->payeeAccount,
+             'notation'=>$this->notation,
+             'amount'=>$this->amount,
+             'currency'=>'USD',
+             'tr_kind_id'=>$act_id,
+          ));
+            $transaction->commit();
+          } catch (Exception $exception) {
+              // Действия по логированию в файл
+              // TO DO
+              
+              $transaction->rollback();
+          }  
+    }
   private function confirmFailureHelper($event){
       $dbObject = Yii::app()->db;
-      $kind_id = false;
-      if(isset($this->transactionId) && is_int($this->transactionId)){
-          $act_id = $this->transactionId;
-      }else{
-        $isExist = $dbObject->createCommand()
-          ->select('kind_id')
-          ->from('pm_transaction_kind')
-          ->where('description=:desc', array(':desc'=>$this->transactionKind))
-          ;
-        $read = $isExist->query();
-        $kind_id = $read->read();
-        if($kind_id == false){
-           $dbObject->createCommand()
-            ->insert('pm_transaction_kind', array('description'=>$this->transactionKind));
-           $act_id = Yii::app()->db->getLastInsertID();
+      $transaction = $dbObject->beginTransaction();
+      try{
+        $kind_id = false;
+        if(isset($this->transactionId) && is_int($this->transactionId)){
+            $act_id = $this->transactionId;
         }else{
-           $act_id = $kind_id['kind_id'];
+          $isExist = $dbObject->createCommand()
+            ->select('kind_id')
+            ->from('pm_transaction_kind')
+            ->where('description=:desc', array(':desc'=>$this->transactionKind))
+            ;
+          $read = $isExist->query();
+          $kind_id = $read->read();
+          if($kind_id == false){
+             $dbObject->createCommand()
+              ->insert('pm_transaction_kind', array('description'=>$this->transactionKind));
+             $act_id = Yii::app()->db->getLastInsertID();
+          }else{
+             $act_id = $kind_id['kind_id'];
+          }
         }
+        $isExist = $dbObject->createCommand()
+            ->select('err_id')
+            ->from('pm_transaction_error')
+            ->where('description=:desc', array(':desc'=>$event->sender->dataOut('ERROR')))
+            ;
+        $read = $isExist->query();
+        $err_id = $read->read();
+        if($err_id == false){
+           $dbObject->createCommand()
+            ->insert('pm_transaction_error', array('description'=>$event->sender->dataOut('ERROR')));
+           $act2_id = Yii::app()->db->getLastInsertID();
+        }else{
+           $act2_id = $err_id['err_id'];
+        }
+        $dbObject->createCommand()
+          ->insert('pm_transaction_log', array(
+             'from_user_id'=>$this->payerId,
+             'from_purse'=>$this->payerAccount,
+             'to_user_id'=>$this->payeeId,
+             'to_purse'=>$this->payeeAccount,
+             'notation'=>$this->notation,
+             'amount'=>$this->amount,
+             'currency'=>'USD',
+             'tr_kind_id'=>$act_id,
+             'tr_err_code'=>$act2_id,
+          ));
+            $transaction->commit();
+      }catch(Exception $exception){
+          // Действия по логированию в файл
+          //TO DO
+          
+          $transaction->rollback();
       }
-      
-      
-      $isExist = $dbObject->createCommand()
-          ->select('err_id')
-          ->from('pm_transaction_error')
-          ->where('description=:desc', array(':desc'=>$event->sender->dataOut('ERROR')))
-          ;
-      $read = $isExist->query();
-      $err_id = $read->read();
-      if($err_id == false){
-         $dbObject->createCommand()
-          ->insert('pm_transaction_error', array('description'=>$event->sender->dataOut('ERROR')));
-         $act2_id = Yii::app()->db->getLastInsertID();
-      }else{
-         $act2_id = $err_id['err_id'];
-      }
-      $dbObject->createCommand()
-        ->insert('pm_transaction_log', array(
-           'from_user_id'=>$this->payerId,
-           'from_purse'=>$this->payerAccount,
-           'to_user_id'=>$this->payeeId,
-           'to_purse'=>$this->payeeAccount,
-           'notation'=>$this->notation,
-           'amount'=>$this->amount,
-           'currency'=>'USD',
-           'tr_kind_id'=>$act_id,
-           'tr_err_code'=>$act2_id,
-        )); 
-  }
+  
+   }
 }
