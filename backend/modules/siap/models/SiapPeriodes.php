b5ae97d2b69534339eaaebe4c561a25ba27ba7b5
andrew <andrew@smart.(none)>
2014-07-18 11:23:04 +0300

    Промежуточная

--- backend/modules/siap/models/SiapPeriodes.php
+++ backend/modules/siap/models/SiapPeriodes.php
@@ -1,21 +1,28 @@
 <?php
 class SiapPeriodes extends CActiveRecord{
-    public $test;
-    public function init(){
-        
+    public $begin;
+    public $end;
+    public function init() {
+        $this->begin = NULL;
+        $this->end = NULL;
     }
     public function tableName() {
        return 'sip_periodes';
     }
     public function rules() {
         return array(
-            array('test', 'type', 'type'=>'integer'),
+            array('begin', 'required'),
+            array('begin', 'date', 'format'=>'yyyy-mm-dd hh:mm'),
         );
+    }
+    public function addWeek(){
+        if($this->begin == NULL)
+            throw new CHttpException('500', 'Error: BEGIN date interval not defined');
+        $this->end = date('Y-m-d H:i', strtotime($this->begin . '+1 WEEK'));
     }
     public static function model($className=__CLASS__)
     {
 	return parent::model($className);
     }
-    
 }
 
--- backend/modules/siap/views/default/index.php
+++
@@ -1,5 +0,0 @@
-<h1> Превед, Медвед!
- <?php echo (isset($model->test)) ? $model->test : '' ?>
- <?php echo (isset($model->begin)) ? $model->begin : '' ?>
- <?php echo (isset($model->end)) ? $model->end : '' ?>
-</h1>
