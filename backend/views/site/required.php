be9fd02c123b75841810a8b57e9488a3e73973e2
andrew <andrew@smart.(none)>
2014-09-09 21:44:50 +0300

    Ликвидация багов, попутное добивание языков

--- backend/views/site/required.php
+++ backend/views/site/required.php
@@ -1,1 +1,4 @@
-<p class="note"><?php echo BaseModule::t('common', 'Fields with {asteriks} are required', array('{asteriks}'=>'<span class="required">*</span>')); ?>.</p>
+<p class="note">
+    <?php //echo BaseModule::t('common', 'Fields with {asteriks} are required', array('{asteriks}'=>'<span class="required">*</span>')).'.'; ?>
+    <?php echo BaseModule::t('rec', 'Fields with * are required'); ?>
+</p>
