9f28830585b77462d0fcb5d02be14e80ef512e86
andrey <andreyantonov78@gmail.com>
2014-09-10 16:01:05 +0300

    желтый квадрат в случае любой ошибки

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -9,7 +9,9 @@ $this->breadcrumbs=array(
 ?>
 
 <!--<div class="error alert alert-error">-->
-<div class="error" style="color: #ffa3a1; margin-top: 50px; margin-bottom: 50px; font-size: 24px;">
-    <h2>Ошибка <?php echo $code; ?></h2>
-    <?php echo CHtml::encode($message); ?>
-</div>
+<!--<div class="error" style="color: #ffa3a1; margin-top: 50px; margin-bottom: 50px; font-size: 24px;">-->
+<!--    <h2>Ошибка --><?php //echo $code; ?><!--</h2>-->
+<!--    --><?php //echo CHtml::encode($message); ?>
+<!--</div>-->
+
+    <div class="error" style="background-color: #ffff5f; width: 700px; height: 500px"></div>
