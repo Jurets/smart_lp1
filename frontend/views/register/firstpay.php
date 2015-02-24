6b9b427912fb4ffc4f8b3ea05ac8e864b1e8b7c0
Jurets <jurets75@gmail.com>
2015-02-24 17:07:19 +0200

    http://mantis.cmapt.com.ua/view.php?id=1548 * добавка формы для платежа на сайте PerfectMOney

--- frontend/views/register/firstpay.php
+++ frontend/views/register/firstpay.php
@@ -2,8 +2,14 @@
 <p id="shag-3-1-text" > <?php echo BaseModule::t('rec', 'To activate your account, you must make the participation fee of $').' '.$ammount_buff ?></p>
 <p class="shag-3-1-sub4"><?php echo BaseModule::t('rec', 'Click this button to login to the PerfectMoney site') ?></p>
 
-<div>
+<!--<div>
     <input type="button" name="btn" id="btn_pay" class="btn-style-blue btn-style-blue-3-1" value="<?php echo BaseModule::t('common', 'PAY $').' '.$ammount_buff ?>" />
-</div>
+</div>-->
 
-<?php $this->renderPartial('pay', array('participant'=>$participant, 'tariff'=>Participant::TARIFF_20), false, true); ?>
+<?php 
+$this->renderPartial('pay', array(
+    'participant'=>$participant, 
+    'tariff'=>Participant::TARIFF_20,
+    'amount'=>$ammount_buff,
+), false, true); 
+?>
--- frontend/views/register/pay.php
+++ frontend/views/register/pay.php
@@ -25,32 +25,76 @@
 </style>
 
 <?php $form = $this->beginWidget('CActiveForm', array(
+        'id'=>'perfectmoney-form',
+        'enableAjaxValidation'=>false,
+        'action'=>'https://perfectmoney.is/api/step1.asp',
+        'method'=>'POST',
+    )); //DebugBreak();
+    $return_url = Yii::app()->request->hostInfo . Yii::app()->request->url;
+    //echo CHtml::activeHiddenField($participant, 'postedActivKey', array('value'=>$participant->activkey)); 
+    //echo CHtml::activeHiddenField($participant, 'tariff_id', array('value'=>$tariff));
+    //echo $form->error($participant, 'tariff_id', array('class'=>'errorpay')); ?>
+
+    <input type="hidden" name="PAYEE_ACCOUNT" value="<?php echo Requisites::purseActivation(); ?>">
+    <input type="hidden" name="PAYEE_NAME" value="JustMoney">
+    <!--<input type="hidden" name="FORCED_PAYER_ACCOUNT" value="<?php echo $participant->purse; ?>">-->
+    <input type="hidden" name="PAYMENT_AMOUNT" value="<?php echo $amount; ?>">
+    <input type="hidden" name="PAYMENT_UNITS" value="USD">
+    <input type="hidden" name="STATUS_URL" value="">
+    <!--<input type="hidden" name="PAYMENT_URL" value="http://jwms.my/register/activate?activkey=<?php echo $participant->activkey;?>">-->
+    <input type="hidden" name="PAYMENT_URL" value="<?php echo $return_url;?>">
+    <input type="hidden" name="NOPAYMENT_URL" value="<?php echo $return_url;?>">
+    <!--<input type="hidden" name="PAYMENT_URL" value="http://justmoney.pro/register/activate?activkey=<?php echo $participant->activkey;?>">
+    <input type="hidden" name="NOPAYMENT_URL" value="http://justmoney.pro/register/activate?activkey=<?php echo $participant->activkey;?>">-->
+    <input type="hidden" name="BAGGAGE_FIELDS" value="">
+    
+    <!--<input type="hidden" name="BAGGAGE_FIELDS" value="ORDER_NUM CUST_NUM">
+    <input type="hidden" name="ORDER_NUM" value="9801121">
+    <input type="hidden" name="CUST_NUM" value="2067609">-->
+    
+    <!--<input type="submit" name="PAYMENT_METHOD" value="PerfectMoney account">-->
+    
+<!--<div id="pm_settings">
+    <?php 
+        //echo CHtml::label(BaseModule::t('rec', 'Enter your Perfect Money data'), '');
+        //echo CHtml::label(BaseModule::t('rec', 'Account') . ':','account', '');
+        //echo CHtml::textField('account','',array('style'=>'width:130px'));
+        //echo CHtml::label(BaseModule::t('rec', 'password') . ':','password', '');
+        //echo CHtml::passwordField('password','',array('style'=>'width:130px'));
+    ?>
+</div>-->
+
+<!--<a href="">
+    <div>-->
+        <!--<input type="button" name="btn" id="btn_pay" class="btn-style-blue btn-style-blue-3-1" value="<?php echo BaseModule::t('common', 'PAY $').' '.$amount ?>" />-->
+        <button type="submit" name="PAYMENT_METHOD" value="PerfectMoney account" class="btn-style-blue btn-style-blue-3-1">
+            <?php echo BaseModule::t('common', 'PAY $').' '.$amount ?>
+        </button>
+    <!--</div>-->
+<?php $this->endWidget(); ?>
+
+    <!--<button type="submit" name="PAYMENT_METHOD" value="PerfectMoney account" class="btn-style-green btn-style-green-2-1" style="cursor: pointer;" id="btn_next">
+        <?php echo BaseModule::t('common', 'Next'); ?>
+    </button>-->
+    
+    <?php 
+$form = $this->beginWidget('CActiveForm', array(
         'id'=>'pay-form',
         'enableAjaxValidation'=>false,
-    )); 
-    echo CHtml::activeHiddenField($participant, 'postedActivKey', array('value'=>$participant->activkey)); 
-    echo CHtml::activeHiddenField($participant, 'tariff_id', array('value'=>$tariff));
-    echo $form->error($participant, 'tariff_id', array('class'=>'errorpay')); ?>
-
-<div id="pm_settings">
-    <?php 
-        echo CHtml::label(BaseModule::t('rec', 'Enter your Perfect Money data'), '');
-        echo CHtml::label(BaseModule::t('rec', 'Account') . ':','account', '');
-        echo CHtml::textField('account','',array('style'=>'width:130px'));
-        echo CHtml::label(BaseModule::t('rec', 'password') . ':','password', '');
-        echo CHtml::passwordField('password','',array('style'=>'width:130px'));
-    ?>
-</div>
-
-<a href="">
-    <?php echo CHtml::submitButton(BaseModule::t('common', 'Next'), array(
+        //'action'=>'https://perfectmoney.is/api/step1.asp',
+        'method'=>'POST',
+    ));    
+    echo CHtml::submitButton(BaseModule::t('common', 'Next'), array(
+    //echo CHtml::button(BaseModule::t('common', 'Next'), array(
             'name'=>'pay',
             'class'=>($participant->tariff_id >= $tariff ? 'btn-style-green btn-style-green-2-1' : 'btn-style-gray'),
+            //'class'=>'btn-style-green btn-style-green-2-1',
             'style'=>'cursor: pointer;',
             'readonly'=>($participant->tariff_id < $tariff),
             'id'=>'btn_next',
-        )); ?>
-</a>
+            //"value"=>"PerfectMoney account",
+    )); ?>
+<!--</a>-->
 
 <?php $this->endWidget(); ?>
 
