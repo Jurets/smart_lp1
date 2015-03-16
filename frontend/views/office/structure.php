ce9919055e31d3b6549d6dc132d9445f3f3eb17c
andrew <volpacchiotto@ukr.net>
2015-03-16 22:14:34 +0200

    еще поправил статистику

--- frontend/views/office/structure.php
+++ frontend/views/office/structure.php
@@ -11,7 +11,7 @@
         <div class='segmentate module2'>
             <a style="background: none;" href="#">
             <div class="photo_wrap">
-              <img class="img-circle" src="<?=(!$model->photo)? '/images/profilLogo.png': '/admin/uploads/origin-'.$model->photo?>">
+              <img class="img-circle" src="<?=(!$model->photo)? '/images/profilLogo.png': Yii::app()->params['upload.url'].'origin-'.$model->photo?>">
               <img class="right_arrow" src="/images/witharrow.png">
             </div>
             <div class="phototext_wrap">
@@ -28,7 +28,7 @@
                     <a style="background: none" href="">
                         <div class="photo_wrap">
                     <div>
-                        <img class="img-circle" src="<?=(is_null($member->photo) || empty($member->photo))? '/images/profilLogo.png': '/admin/uploads/origin-'.$member->photo?>">
+                        <img class="img-circle" src="<?=(is_null($member->photo) || empty($member->photo))? '/images/profilLogo.png': Yii::app()->params['upload.url'].'/origin-'.$member->photo?>">
                         <img class="right_arrow" src="/images/witharrow.png">
                     </div>
                         <div class="phototext_wrap"><p><?=$member->first_name.' '.$member->last_name?></p></div>
