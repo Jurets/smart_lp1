313ec4f3ab43f5f6bc443757b62e87728dc8086f
andrew <volpacchiotto@ukr.net>
2015-03-30 19:55:36 +0300

    дату создания изменил на дату активации как просил Михаил

--- frontend/views/office/newsone.php
+++ frontend/views/office/newsone.php
@@ -3,7 +3,7 @@
          
         <div id="office-3-1post1">
             <?php echo TbHtml::tag('div', array('style'=>'background-image: url(/superjust/uploads/'.'origin-'.$model->image, 'id'=>'blogImg1'), '&nbsp;'); ?>
-            <h4 class="office-3-miniZagolovok"><?php echo Yii::app()->dateFormatter->formatDateTime($model->created, 'medium', null) .', '. Yii::app()->dateFormatter->formatDayInWeek('EEEE', array('wday'=>date('N', strtotime($model->created))));?></h4>
+            <h4 class="office-3-miniZagolovok"><?php echo Yii::app()->dateFormatter->formatDateTime($model->activated, 'medium', null) .', '. Yii::app()->dateFormatter->formatDayInWeek('EEEE', array('wday'=>date('N', strtotime($model->activated))));?></h4>
             <p class="office-3-zagolovok"><span style="text-transform:uppercase;"><?php echo $model->title ?></span></p>
             <p class="office-3-text">
                 <?php echo $model->content ?>
